c3dad102f354f793fcaa39e842b3d1c487ca674d
kryban93 <kryban1993@gmail.com>
2022-04-03 21:27:32 +0200

    Added working shopping list based on input references

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       'Create some spagetti bla bla',
       'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
     ),
+    new Recipe(
+      'Ramen',
+      'Create some ramen with this extra recipe bla bla',
+      'https://images.unsplash.com/photo-1591814468924-caf88d1232e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    ),
   ];
 
   constructor() {}
